3e897ca23740304227b7332ffe13fc19ce29b94e
Sergey-Akmalov <akmolgt@gmail.com>
2017-10-07 16:21:51 +0300

    display sent, important and trashed messages

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -18,19 +18,19 @@
 
       <li :class="{ active: activeView == 'app-sent' }">
         <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
-          <i class="fa fa-envelope-o"></i>Sent <span class="label label-default pull-right">{{ sentMessages.length }}</span>
+          <i class="fa fa-envelope-o"></i>Sent <span class="label label-default pull-right">?</span>
         </a>
       </li>
 
       <li :class="{ active: activeView == 'app-important' }">
         <a href="#" @click.prevent="navigate('app-important', 'Important')">
-          <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">{{ importantMessages.length }}</span>
+          <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">?</span>
         </a>
       </li>
 
       <li :class="{ active: activeView == 'app-trash' }">
         <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
-          <i class=" fa fa-trash-o"></i>Trash <span class="label label-default pull-right">{{ trashedMessages.length }}</span>
+          <i class=" fa fa-trash-o"></i>Trash <span class="label label-default pull-right">?</span>
         </a>
       </li>
     </ul>
